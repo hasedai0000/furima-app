643e07f59af9cc3547ca584f97d93132c1bb5300
daichi-hasegawa-styleedge <d.hasegawa@styleedge.co.jp>
2025-08-01 09:29:27 +0900

    認証時のバリデーションの修正とスタイルの修正

---
+++ src/resources/views/auth/verify-email.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.app')
+
+@section('css')
+  <link rel="stylesheet" href="{{ asset('css/login.css') }}">
+@endsection
+
+@section('content')
+  <div class="login-form__content">
+    <div class="login-form__heading">
+      <h2 class="login-form__heading-title">メール認証</h2>
+    </div>
+    <div class="form__group">
+      <p>メールアドレスの認証が必要です。</p>
+      <p>登録時に送信されたメールのリンクをクリックして認証を完了してください。</p>
+    </div>
+    <div class="form__button">
+      <a href="{{ route('verification.notice') }}">メール認証はこちら</a>
+    </div>
+    <div class="form__button">
+      <form method="POST" action="{{ route('verification.send') }}">
+        @csrf
+        <button class="form__button-submit" type="submit">認証メールを再送信</button>
+      </form>
+    </div>
+  </div>
+@endsection
